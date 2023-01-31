d86bc0db22dd7215c3c01d120ddd4cd0dc369eaa
vipersak <124049980+vipersak@users.noreply.github.com>
2023-01-31 13:51:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("function1").addEventListener("click", function() {
-      document.getElementById("display").innerHTML = "Function 1 selected";
+      document.getElementById("display").innerHTML = "A SMS will  be Sent at button press";
     });
   
     document.getElementById("function2").addEventListener("click", function() {
-      document.getElementById("display").innerHTML = "Function 2 selected";
+      document.getElementById("display").innerHTML = "An Email will  be Sent at button press";
     });
   
     document.getElementById("function3").addEventListener("click", function() {
-      document.getElementById("display").innerHTML = "Function 3 selected";
+      document.getElementById("display").innerHTML = "Alarm will be set at button press";
     });
   });
