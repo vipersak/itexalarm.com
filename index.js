8f7234cc8b8cdcfeae5e9abdd54dabddbae32e7b
vipersak <124049980+vipersak@users.noreply.github.com>
2023-02-01 12:14:46 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("function1").addEventListener("click", function() {
-      document.getElementById("display").innerHTML = "A SMS will  be Sent at button press";
+      document.getElementById("display").innerHTML = "SMS will be sent with button";
     });
   
     document.getElementById("function2").addEventListener("click", function() {
-      document.getElementById("display").innerHTML = "An Email will  be Sent at button press";
+      document.getElementById("display").innerHTML = "Email will be sent with button";
     });
   
     document.getElementById("function3").addEventListener("click", function() {
-      document.getElementById("display").innerHTML = "Alarm will be set at button press";
+      document.getElementById("display").innerHTML = "Alarm will be set with button";
+    });
+    document.getElementById("function4").addEventListener("click", function() {
+      document.getElementById("display").innerHTML = "SMS will be sent by NFC";
+    });
+  
+    document.getElementById("function5").addEventListener("click", function() {
+      document.getElementById("display").innerHTML = "Email will be sent by NFC";
+    });
+  
+    document.getElementById("function6").addEventListener("click", function() {
+      document.getElementById("display").innerHTML = "Alarm will be set by NFC";
     });
   });
